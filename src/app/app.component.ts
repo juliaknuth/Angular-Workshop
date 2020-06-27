296b7711b0c2a04a1297a7c8149570498ad73915
Julia <julia@MacBook-Pro.fritz.box>
2020-06-27 11:53:35 +0200

    Add submit logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TodoItem } from './interfaces/todo-item';
 
 @Component({
   selector: 'app-root',
@@ -7,18 +8,19 @@ import { Component } from '@angular/core';
     Welcome to {{ title }}!
   </h1>
 
-  <app-input-button-unit></app-input-button-unit>
+  <app-input-button-unit (submit)="addItem($event)"></app-input-button-unit>
   <ul>
     <li *ngFor="let todoItem of todoList">
-      {{ todoItem.title }}
+
+      <app-todo-item [item]="todoItem"></app-todo-item>
     </li>
   </ul>
 `,
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'todo-list';
-  todoList = [
+  title = 'your todo list';
+  todoList: TodoItem[] = [
     {title: 'install NodeJS'},
     {title: 'install Angular CLI'},
     {title: 'create new app'},
@@ -27,6 +29,10 @@ export class AppComponent {
     {title: 'develop app'},
     {title: 'deploy app'},
   ];
+
+  addItem(title: string) {
+    this.todoList.push({ title });
+  }
 }
 
 
--- src/app/input-button-unit/input-button-unit.component.ts
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 
 @Component({
@@ -7,13 +7,16 @@ import { Component, OnInit } from '@angular/core';
 
     <input #inputElementRef
     [value]="title"
-    (keyup.enter)="changeTitle($event.target.value)">
-    <button (click)="changeTitle('Button clicked!')">Save</button>
+    (keyup.enter)="submitValue($event.target.value)">
+    <button (click)="submitValue(inputElementRef.value)">Save</button>
   `,
 
   styleUrls: ['./input-button-unit.component.scss']
 })
 export class InputButtonUnitComponent implements OnInit {
+
+  @Output() submit: EventEmitter<string> = new EventEmitter();
+
   title = 'Hello World';
 
   constructor() { }
@@ -21,8 +24,8 @@ export class InputButtonUnitComponent implements OnInit {
   ngOnInit() {
   }
 
-  changeTitle(newTitle: string) {
-    this.title = newTitle;
+  submitValue(newTitle: string) {
+    this.submit.emit(newTitle);
   }
 
 }
