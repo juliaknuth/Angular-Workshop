2fcc23eb28c204eae10363084291875bc66be26b
Julia <julia@MacBook-Pro.fritz.box>
2020-06-27 13:34:46 +0200

    Outsource list to a component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,17 @@
 import { Component } from '@angular/core';
-import { TodoItem } from './interfaces/todo-item';
 
 @Component({
   selector: 'app-root',
   template: `
-  <h1>
-    Welcome to {{ title }}!
-  </h1>
-
-  <app-input-button-unit (submit)="addItem($event)"></app-input-button-unit>
-  <ul>
-    <li *ngFor="let todoItem of todoList">
-
-      <app-todo-item [item]="todoItem"></app-todo-item>
-    </li>
-  </ul>
-`,
-  styleUrls: ['./app.component.scss']
+    <h1>
+      Welcome to {{ title }}!
+    </h1>
+  `,
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'your todo list';
-  todoList: TodoItem[] = [
-    {title: 'install NodeJS'},
-    {title: 'install Angular CLI'},
-    {title: 'create new app'},
-    {title: 'push to GitHub'},
-    {title: 'serve app'},
-    {title: 'develop app'},
-    {title: 'deploy app'},
-  ];
-
-  addItem(title: string) {
-    this.todoList.push({ title });
-  }
+  title = 'My To Do List APP';
 }
+
 
 
---
+++ src/app/list-manager/list-manager.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { TodoItem } from '../interfaces/todo-item';
+
+@Component({
+  selector: 'app-root',
+  template: `
+  <h1>
+    Welcome to {{ title }}!
+  </h1>
+
+  <app-input-button-unit (submit)="addItem($event)"></app-input-button-unit>
+  <ul>
+    <li *ngFor="let todoItem of todoList">
+      <app-todo-item [item]="todoItem"></app-todo-item>
+    </li>
+  </ul>
+`,
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'your todo list';
+  todoList: TodoItem[] = [
+    {title: 'install NodeJS'},
+    {title: 'install Angular CLI'},
+    {title: 'create new app'},
+    {title: 'push to GitHub'},
+    {title: 'serve app'},
+    {title: 'develop app'},
+    {title: 'deploy app'},
+  ];
+
+  addItem(title: string) {
+    this.todoList.push({ title });
+  }
+}
+
